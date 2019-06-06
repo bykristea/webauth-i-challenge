7adf93bb994b967d763f013c3a8cfad5ee66e8ea
Kristea Gamel <krgamel@gmail.com>
2019-06-05 20:00:27 -0500

    creating basic file structure
    also edited knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,26 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/auth.db3',
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
+  
+  pool: {
       min: 2,
       max: 10
     },
+    
     migrations: {
       tableName: 'knex_migrations'
-    }
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
+
+
